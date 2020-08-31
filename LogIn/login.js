1b9e6edd7ad4336b224c942940e74f3303cc8d62
hyejun <hyejun0608@naver.com>
2020-08-31 13:15:48 +0900

    LogIn
    로그인페이지

---
+++ LogIn/login.js
@@ -0,0 +1,32 @@
+const id = document.querySelector(".id");
+const pw = document.querySelector(".pw");
+const login = document.querySelector(".login");
+
+function loginCheck() {
+    if ((id.value) === "") {
+        alert("아이디를 입력해주세요!");
+        id.focus();
+        return false;
+    }
+    if ((pw.value) === "") {
+        alert("비밀번호를 입력해주세요!");
+        pw.focus();
+        return false;
+    }
+    login.submit;
+}
+function onClicked() {
+    loginCheck();
+    axios.post('http://52.78.41.100:8000/user/login', {
+        data: {
+            "userId": "id.value",
+            "password": "pw.value"
+        }
+    })
+    .then (
+        alert("로그인 성공했습니다!")
+    )
+    .catch(error => {
+        console.log(error);
+    })
+  }
